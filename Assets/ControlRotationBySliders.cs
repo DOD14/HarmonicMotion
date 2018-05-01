ce2e8ce82cdec44e06c52b0204f1c03a34804c73
DOD14 <38224694+DOD14@users.noreply.github.com>
2018-05-01 18:55:55 +0300

    Rotation added

---
+++ Assets/ControlRotationBySliders.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlRotationBySliders : MonoBehaviour {
+
+    public Slider[] sliders;
+
+	private void Start()
+	{
+        AdjustRotation();
+	}
+	// Update is called once per frame
+	public void AdjustRotation () {
+
+        transform.rotation = Quaternion.Euler(sliders[0].value, sliders[1].value, sliders[2].value);
+	}
+}
